579981f6a968d1d0819f42db9c5fe0f0d9a44921
FlubbytheMonki <161659891+FlubbytheMonki@users.noreply.github.com>
2026-06-25 11:41:08 -0400

    added ip location based weather and state and country fields

--- script.js
+++ script.js
@@ -2,18 +2,31 @@ const apiKey = '8584f75553ee5fbf67c83208c37ab4c2';
 const units = 'imperial'; // or 'imperial'
 const cnt = 5; // number of forecast entries
 
-async function fetchWeatherData() { 
-    const city = document.getElementById('cityInput').value;
-    console.log(`city: ${city}`)
+async function fetchWeatherData() {
+    const response = await fetch('http://ip-api.com/json');
+    const ipData = await response.json();
+
+    let city = document.getElementById('cityInput').value.trim();
+    let state = document.getElementById('stateInput').value.trim();
+    let country = document.getElementById('countryInput').value.trim();
+
+    if (!city) city = ipData.city;
+    if (!state) state = ipData.region;
+    if (!country) country = ipData.countryCode;
+
+    console.log(ipData.city)
+    console.log(ipData.region)
+    console.log(ipData.countryCode)
+
     try {
         
-        const response = await fetch(
-            `https://api.openweathermap.org/data/2.5/forecast?q=${(city)}&appid=${(apiKey)}&units=${units}&cnt=${cnt}`
+        const responseweather = await fetch(
+            `https://api.openweathermap.org/data/2.5/forecast?q=${(city)},${(state)},${(country)}&appid=${(apiKey)}&units=${units}&cnt=${cnt}`
         );
 
-        if (!response.ok) {
-            console.error('API error', response.status);
-            if (response.status = 404) {
+        if (!responseweather.ok) {
+            console.error('API error', responseweather.status);
+            if (responseweather.status === 404) {
                 console.error("data field is incorrect")
                 document.getElementById('error').innerText = 'error: data field is incorrect. please recheck your city for misspellings and try again';
             }
@@ -21,7 +34,7 @@ async function fetchWeatherData() {
         }
 
         document.getElementById('error').innerText = '';
-        const data = await response.json();
+        const data = await responseweather.json();
         
         const temp = Math.floor(data.list[0].main.temp)
         const icon = data.list[0].weather[0].icon;
@@ -30,7 +43,8 @@ async function fetchWeatherData() {
         const humidity = data.list[0].main.humidity;
         console.log('weather data', Math.floor(data.list[0].main.temp));
         document.getElementById('weather').innerHTML = `<img src="https://openweathermap.org/img/wn/${icon}@2x.png">
-    <h3>${temp}°F   ${cityName}   current humidity: ${humidity}   ${description}</h3>`;
+    <h3>${temp}°F   ${cityName}   <div>current humidity: ${humidity} </div>
+    ${description}</h3>`;
     } catch (error) {
         console.error('Error:', error);
     }
